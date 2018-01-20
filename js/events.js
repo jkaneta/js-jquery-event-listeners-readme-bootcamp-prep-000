ee72e8b5a24ee07c41e4434d804e87631205a5e2
J <flatironschool@2joyce.com>
2018-01-20 09:54:36 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,10 +7,13 @@ function frameIt() {
   $('img').addClass('tasty');
 }
 
-function pressIt(e) {
-  if (e.which == 71) {
-    alert('g was pressed.');
-  }
+function pressIt() {
+  $('input[type=text]').on('keydown', function(e) {
+    if (e.which == 71) {
+      alert('g was pressed.');
+    }
+  });
+  
   console.log('pressit called');
   console.log(this);
 }
@@ -26,7 +29,7 @@ $('p').on('click', getIt);
 
 $(window).on('load', frameIt);
 
-$('input[type=text]').on('keydown', pressIt);
+
 
 $('form').on('submit', submitIt);
   
