36f178c8e992bc6aa463a9eecc26652abccb8158
J <flatironschool@2joyce.com>
2018-01-20 08:41:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  alert('Hey!');
+}
+
+function frameIt() {
+  $('img').addClass('tasty');
+}
+
+function pressIt() {
+  if ($('#target').keyCode == 71) {
+    alert('g was pressed.');
+  }
+  console.log($(this), $(this).nodeName);
+}
+
+function submitIt() {
+  alert('Your form is going to be submitted now.');
+}
 
 $(document).ready(function(){
 
 // call functions here
+$('p').on('click', getIt);
 
+$(window).on('load', frameIt);
+
+$('#typing').on('keydown', pressIt);
+
+$('form').on('submit', submitIt);  
+  
 });
+
+
